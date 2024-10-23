8925ca190a48335a37955321e51816add2da3d63
ludesmo <lu.paredesm@duocuc.cl>
2024-10-23 14:16:13 -0300

    panel_control

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegistroComponent } from './pages/auth/registro/registro.component';
 import { HomeComponent } from './pages/home/home/home.component';
-<<<<<<< HEAD
 import { EmergenciaComponent } from './pages/emergencia/emergencia.component';
-=======
 import { PanelControlComponent } from './pages/panel-control/panel-control.component';
->>>>>>> c22d13a3b6398993e45fa8c5ec3f2f65f0b1e5d5
 
 export const routes: Routes = [
     {
@@ -27,13 +24,12 @@ export const routes: Routes = [
         component: HomeComponent
     },
     {
-<<<<<<< HEAD
         path:'emergencia',
         component: EmergenciaComponent
-=======
+    },
+    {
         path: 'panel-control',
         component: PanelControlComponent
->>>>>>> c22d13a3b6398993e45fa8c5ec3f2f65f0b1e5d5
     },
     {
         path:'**',
--- src/app/pages/panel-control/panel-control.component.ts
+++ src/app/pages/panel-control/panel-control.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-panel-control',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './panel-control.component.html',
   styleUrl: './panel-control.component.css'
 })
-export class PanelControlComponent {
+export class PanelControlComponent implements OnInit {
+
+  emergenciaForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.emergenciaForm = this.fb.group({
+      descripcion: ['', Validators.required],
+      victimas: ['', Validators.required],
+      tipo: ['escoja', Validators.required],
+      tipoArea: ['escoja', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.emergenciaForm.valid) {
+      console.log(this.emergenciaForm.value);
+      alert('Emergencia registrada exitosamente!');
+      this.emergenciaForm.reset();
+    } else {
+      alert('Por favor, complete todos los campos.');
+    }
+  }
 
 }
