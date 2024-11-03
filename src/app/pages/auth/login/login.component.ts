e932f21e608e4161b4c204aa6782733bedcc42a8
MartinPPY <martinsantiago.se@gmail.com>
2024-11-03 01:35:25 -0300

    agregando recepcion

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth/auth.service';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MensajeriaService } from '../../../services/mensajeria/mensajeria.service';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +16,7 @@ export class LoginComponent {
   cargando?: boolean
   tipoUsuario?: string
 
-  constructor(private _auth: AuthService, private fb: FormBuilder) {
+  constructor(private _auth: AuthService, private fb: FormBuilder, private mensajeria: MensajeriaService) {
 
     this.loginForm = fb.group({
       correo: ['', [Validators.required, Validators.email]],
@@ -27,30 +28,47 @@ export class LoginComponent {
 
 
   login() {
-    setTimeout(() => {
 
-      this.cargando = false
+    this.cargando = false
 
-      const data = {
+    const data = {
 
-        email: this.loginForm.get('correo').value,
-        password: this.loginForm.get('password').value,
+      email: this.loginForm.get('correo').value,
+      password: this.loginForm.get('password').value,
 
+    }
+
+    this._auth.login(data).subscribe({
+      next: (data) => {
+        this.tipoUsuario = data.user.cargo
+        this._auth.verificarTipoUsuario(this.tipoUsuario!)
+      },
+      error: (err) => {
+        console.log(err)
       }
+    })
 
-      this._auth.login(data).subscribe({
-        next: (data) => {
-          this.tipoUsuario = data.user.cargo
-          this._auth.verificarTipoUsuario(this.tipoUsuario!)
-        },
-        error: (err) => {
-          console.log(err)
-        }
-      })
-
-    }, 3000);
-    this.cargando = true
   }
+
+  validarCampo(nombre: string): boolean {
+    return this.loginForm.get(nombre).errors && this.loginForm.get(nombre).touched
+  }
+
+  validarCampos(): boolean {
+
+    const campos = Object.keys(this.loginForm.controls)
+
+    for (let index = 0; index < campos.length; index++) {
+      const campo = this.loginForm.get(campos[index])
+      if (campo.errors) {
+        this.cargando = false
+        this.mensajeria.presentarAlerta('Verifica los campos!')
+        return false
+      }
+    }
+    return true
+  }
+
 
 
 
